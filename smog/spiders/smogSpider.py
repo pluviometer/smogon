188dcc88c49ed82a2007fece187edd67afdafd4b
pluviometer <31447637+pluviometer@users.noreply.github.com>
2018-01-04 20:29:56 -0800

    simplify url input

--- smog/spiders/smogSpider.py
+++ smog/spiders/smogSpider.py
@@ -6,7 +6,8 @@ from smog.items import SmogItem
 class smogSpider(scrapy.Spider):
     name = 'spinarak'
     allowed_domains = ["smogon.com"]
-    start_urls = ["http://www.smogon.com/forums/threads/smogon-exhibition-signups.3623346/"]
+    main_url = "http://www.smogon.com/forums/threads/smogon-exhibition-signups.3623346/"
+    start_urls = [main_url]
 
     def __init__(self):
         self.i = 1
@@ -17,12 +18,10 @@ class smogSpider(scrapy.Spider):
             for sel in response.xpath("//div/article[@class = 'message message--post js-post js-inlineModContainer  ']"):
                 item = SmogItem()             
                 item['num'] = sel.xpath(".//div[@class = 'message-permalink']/a/text()").re(r'(\d+)')
-                #item['time'] = sel.xpath(".//time/@datetime").extract()
                 item['user'] = sel.xpath(".//@data-author").extract()
-                item['text'] = sel.xpath(".//div[@class = 'bbWrapper']//text()").extract()
-                #item['Spe'] = sel.xpath(".//td[@style = 'background:#FA92B2']/text()").re(r'(\d+)')         
+                item['text'] = sel.xpath(".//div[@class = 'bbWrapper']//text()").extract()      
                 yield item
             
-            url = "http://www.smogon.com/forums/threads/smogon-exhibition-signups.3623346/page-" + str(self.i + 1) 
+            url = main_url + "page-" + str(self.i + 1) 
             yield scrapy.Request(url, callback = self.parse)
         self.i += 1
