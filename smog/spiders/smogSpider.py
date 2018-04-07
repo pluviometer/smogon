d156ed0920cf7660cfcf5ca55fd1eed25e8d790b
pluviometer <31447637+pluviometer@users.noreply.github.com>
2018-04-07 00:52:06 -0400

    update with link, time2

--- smog/spiders/smogSpider.py
+++ smog/spiders/smogSpider.py
@@ -3,7 +3,7 @@ import re
 import scrapy
 from smog.items import SmogItem
 
-main_url = "http://www.smogon.com/forums/threads/official-smogon-tournament-xiv-signups.3625049/"
+main_url = "https://www.smogon.com/forums/threads/underused-premier-league-vi-player-signups.3631034/"
 
 class smogSpider(scrapy.Spider):
     name = 'spinarak'
@@ -19,9 +19,11 @@ class smogSpider(scrapy.Spider):
             for sel in response.xpath("//div/article[@class = 'message message--post js-post js-inlineModContainer  ']"):
                 item = SmogItem()             
                 item['num'] = sel.xpath(".//div[@class = 'message-permalink']/a/text()").re(r'(\d+)')
+                item['link'] = sel.xpath(".//a[@class = 'message-attribution-main u-concealed']/@href").extract()
                 item['time'] = sel.xpath(".//header[@class = 'message-attribution']/a[@rel = 'nofollow']/time[@class = 'u-dt']/@title").extract()
+                item['time2'] = sel.xpath(".//header[@class = 'message-attribution']/a[@rel = 'nofollow']/time/@datetime").extract()
                 item['user'] = sel.xpath(".//@data-author").extract()
-                #item['text'] = sel.xpath(".//div[@class = 'bbWrapper']//text()").extract()     
+                item['text'] = sel.xpath(".//div[@class = 'bbWrapper']//text()").extract()     
                 yield item
             
             url = main_url + "page-" + str(self.i + 1) 
